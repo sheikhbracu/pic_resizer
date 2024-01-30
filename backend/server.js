b413b5611f0e911a6ddf573af5ee433752bb24f4
Sheikh Sadi <sheikhsadiemon01@gmail.com>
2024-01-30 21:55:37 +0600

    final project

---
+++ backend/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const path = required('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Multer storage configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'No file uploaded' });
+        }
+
+        const { width, height, format } = req.body;
+
+        if (!width || !height || !format) {
+            return res.status(400).json({ error: 'Missing parameters' });
+        }
+
+        // const resizedImage = await sharp(req.file.buffer)
+        //     .resize(parseInt(width), parseInt(height))
+        //     .toFormat(format)
+        //     .toBuffer();
+        const resizedImage = await sharp(req.file.buffer)
+    .resize({
+        width: parseInt(width),
+        height: parseInt(height),
+
+        fit: 'fill', // Resize and stretch to exactly match the input width and height
+        position: 'centre', // Position the resized image at the center
+
+        
+        // fit: 'inside', // Resize to fit within the specified dimensions without cropping
+        // withoutEnlargement: true, // Don't enlarge the image if its dimensions are smaller than specified
+    })
+    .toFormat(format)
+    .toBuffer();
+
+        // Save the resized image to disk or database
+        // For demonstration, we'll just send it as a response
+        // res.set({ 'Content-Type': `image/${format}` });
+        res.set({
+            'Content-Type': `image/${format}`,
+            'Content-Disposition': 'inline', // Display in browser
+            'Content-Length': resizedImage.length,
+        });
+        // Send the resized image as the response
+        return res.send(resizedImage);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './style.css'; // Import CSS file
+
+const App = () => {
+    const [file, setFile] = useState(null);
+    const [width, setWidth] = useState('');
+    const [height, setHeight] = useState('');
+    const [format, setFormat] = useState('png');
+    const [resizedImage, setResizedImage] = useState(null);
+    const [originalWidth, setOriginalWidth] = useState(null);
+    const [originalHeight, setOriginalHeight] = useState(null);
+    const [fileSize, setFileSize] = useState(null);
+    const [resizedFileSize, setResizedFileSize] = useState(null);
+    const [maxFileSizeKB, setMaxFileSizeKB] = useState(null);
+
+    const handleFileChange = (e) => {
+        const uploadedFile = e.target.files[0];
+        setFile(uploadedFile);
+
+        // Fetch original image dimensions
+        const image = new Image();
+        image.src = URL.createObjectURL(uploadedFile);
+        image.onload = () => {
+            setOriginalWidth(image.width);
+            setOriginalHeight(image.height);
+        };
+
+        // Fetch file size
+        setFileSize(uploadedFile.size);
+    };
+
+    const handleWidthChange = async (e) => {
+        const newWidth = parseInt(e.target.value);
+        setWidth(newWidth);
+
+        // Calculate height based on aspect ratio
+        const aspectRatio = originalWidth / originalHeight;
+        const newHeight = Math.round(newWidth / aspectRatio);
+        setHeight(newHeight);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('image', file);
+        formData.append('width', width);
+        formData.append('height', height);
+        formData.append('format', format);
+
+        // If format is jpg and max file size is not provided, default to PNG file size
+        if (format === 'jpg' && !maxFileSizeKB) {
+            const pngMaxFileSizeKB = Math.round(fileSize / 1024);
+            formData.append('maxFileSizeKB', pngMaxFileSizeKB);
+        } else if (format === 'jpg' && maxFileSizeKB) {
+            formData.append('maxFileSizeKB', maxFileSizeKB);
+        }
+
+        try {
+            const response = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                responseType: 'blob',
+            });
+
+            const imageUrl = URL.createObjectURL(response.data);
+            setResizedImage(imageUrl);
+
+            // Fetch resized image file size in kilobytes
+            const resizedBlob = response.data;
+            const resizedBlobSize = resizedBlob.size;
+            const resizedFileSizeKB = Math.round(resizedBlobSize / 1024);
+            setResizedFileSize(resizedFileSizeKB);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getDownloadFilename = () => {
+        if (format === 'jpg') {
+            return 'resized_image.jpg';
+        } else if (format === 'png') {
+            return 'resized_image.png';
+        } else if (format === 'bmp') {
+            return 'resized_image.bmp';
+        }
+        // Add more file format conditions as needed
+    };
+
+    return (
+        <div className="container">
+            <h1>Image Resizer</h1>
+            <p>It does not store any data after resizing.</p>
+            
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileChange} />
+                {originalWidth && originalHeight && (
+                    <p>Original Pixel Size: {originalWidth} x {originalHeight}</p>
+                )}
+                {fileSize && (
+                    <p>Original File Size: {Math.round(fileSize / 1024)} KB</p>
+                )}
+                <div className="input-row">
+                    <input
+                        type="text"
+                        placeholder="Width"
+                        value={width}
+                        onChange={handleWidthChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Height"
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                    />
+                    <select value={format} onChange={(e) => setFormat(e.target.value)}>
+                        <option value="png">PNG</option>
+                        <option value="jpg">JPG</option>
+                        <option value="bmp">BMP</option>
+                        {/* Add more format options as needed */}
+                    </select>
+                    
+                </div>
+                {/* {format === 'jpg' && (
+                    <input
+                        type="text"
+                        placeholder="Max File Size (KB)"
+                        value={maxFileSizeKB}
+                        onChange={(e) => setMaxFileSizeKB(e.target.value)}
+                    />
+                )} */}
+                <button type="submit">Resize and Save</button>
+            </form>
+            {resizedImage && (
+                <div className="resized-image-container">
+                    <h2>Resized Image:</h2>
+                    <img src={resizedImage} alt="Resized" />
+                    <p>Resized File Size: {resizedFileSize} KB</p>
+                    <a href={resizedImage} download={getDownloadFilename()}>Download Resized Image</a>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default App;
