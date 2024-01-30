730cde55baa27c3d180a34200e0b09baf56296ea
sheikhbracu <102944927+sheikhbracu@users.noreply.github.com>
2024-01-30 22:39:44 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,45 +22,37 @@ app.post('/upload', upload.single('image'), async (req, res) => {
             return res.status(400).json({ error: 'Missing parameters' });
         }
 
-        // const resizedImage = await sharp(req.file.buffer)
-        //     .resize(parseInt(width), parseInt(height))
-        //     .toFormat(format)
-        //     .toBuffer();
         const resizedImage = await sharp(req.file.buffer)
-    .resize({
-        width: parseInt(width),
-        height: parseInt(height),
+            .resize({
+                width: parseInt(width),
+                height: parseInt(height),
+                fit: 'fill', // Resize and stretch to exactly match the input width and height
+                position: 'centre', // Position the resized image at the center
+            })
+            .toFormat(format)
+            .toBuffer();
 
-        fit: 'fill', // Resize and stretch to exactly match the input width and height
-        position: 'centre', // Position the resized image at the center
-
-        
-        // fit: 'inside', // Resize to fit within the specified dimensions without cropping
-        // withoutEnlargement: true, // Don't enlarge the image if its dimensions are smaller than specified
-    })
-    .toFormat(format)
-    .toBuffer();
-
-        // Save the resized image to disk or database
-        // For demonstration, we'll just send it as a response
-        // res.set({ 'Content-Type': `image/${format}` });
         res.set({
             'Content-Type': `image/${format}`,
             'Content-Disposition': 'inline', // Display in browser
             'Content-Length': resizedImage.length,
         });
-        // Send the resized image as the response
+
         return res.send(resizedImage);
     } catch (err) {
         console.error(err);
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+
+// Serve static files
+const frontendBuildPath = path.join(__dirname, 'frontend', 'build');
+app.use(express.static(frontendBuildPath));
+
+// Serve the React app for any other requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendBuildPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
